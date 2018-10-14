f42b726bfd9adeaacf4a73389046762226fe8515
HosniAmmar <hosniammar123@gmail.com>
2018-10-14 11:59:22 +0100

    first commit

---
+++ src/main/java/isetch/winners/apifme/ApiFmeApplication.java
@@ -0,0 +1,12 @@
+package isetch.winners.apifme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFmeApplication.class, args);
+	}
+}
